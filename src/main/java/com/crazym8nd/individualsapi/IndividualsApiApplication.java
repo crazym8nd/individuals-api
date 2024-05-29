6ff72c5b00bf07cb4b4733d4e645a98a4958e224
crazymind <no1me@bk.ru>
2024-05-29 15:04:26 +0300

    changed keycloak version to 24

--- src/main/java/com/crazym8nd/individualsapi/IndividualsApiApplication.java
+++ src/main/java/com/crazym8nd/individualsapi/IndividualsApiApplication.java
@@ -2,6 +2,7 @@ package com.crazym8nd.individualsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 public class IndividualsApiApplication {
